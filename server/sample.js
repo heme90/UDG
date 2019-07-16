591ec3ecb3f731ce0076a77c2c61fec1cbe10a9a
heme90 <dbsrl0423@gmail.com>
2019-07-16 15:42:42 +0900

    no message

--- server/sample.js
+++ server/sample.js
@@ -34,42 +34,6 @@ var server = http.createServer(function (req, res) {   //create web server
     }
 });
 
-/*
-
-    if (_url = '/' ) {
-
-        res.end(fs.readFileSync('C:\\MyNode\\nodetest\\index.html','utf-8'))
-    
-    }
-    
-    if (_url = '/comment' ) {
-       
-        res.write('<h1>지도 주석 페이지의 html입니다</h1><br/>');
-    
-    } 
-    
-    if (_url = '/comment.do' ) {
-        
-        res.write('<h1>지도 주석 페이지의 실행 스크립트입니다</h1><br/>'
-        + req['comm']+ ' 을 주석으로 등록합니다');
-    
-    }
-    
-    if (_url = '/share.do' ) {
-        
-        res.write('<h1>지도 공유 페이지의 실행 스크립트입니다</h1><br/>'
-        + req['user'] + ' 의 ' + req['mapnum'] + ' 번 지도를 공유합니다');
-    
-    }
-    
-    if (_url = '/follow.do' ) {
-      
-        res.write('<h1>지도 팔로우 페이지의 실행 스크립트 입니다</h1><br/>'
-        + req['globalmapnum'] + ' 번 지도를 팔로우합니다');
-    
-    }
-*/
-
 
 server.listen(1337,'localhost'); //6 - listen for any incoming requests
 
