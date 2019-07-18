a8afe31d440eee2ed705cab7186a7be04b5c2a58
heme90 <dbsrl0423@gmail.com>
2019-07-18 16:41:11 +0900

    서버 로그인 구현 모듈 재설치

---
+++ node_modules/Server/models/token.js
@@ -0,0 +1,9 @@
+var mongoose = require('mongoose');
+
+var TokenSchema   = new mongoose.Schema({
+  value: { type: String, required: true },
+  userId: { type: String, required: true },
+  clientId: { type: String, required: true }
+});
+
+module.exports = mongoose.model('Token', TokenSchema);
